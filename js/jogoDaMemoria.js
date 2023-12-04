6323a732570e5063ebf48442e71357d3cb1b8113
Matheus Freitas <140122752+MatheusHenriqueNF@users.noreply.github.com>
2023-12-04 17:25:41 -0300

    tempo ao vencer e caracteres de login

--- js/jogoDaMemoria.js
+++ js/jogoDaMemoria.js
@@ -33,7 +33,7 @@ const createElement = (tag, className) =>{
 /*exibir a mensagem que ganhou e volta para a pagina inicial*/
 const Alerta = () => {
     Swal.fire({
-        text:   'Parabéns',
+        text:   `Parabén, seu tempo é de ${timer.innerHTML} segundos`,
         title: 'Venceu!',
     }).then((resultado) =>{
         if(resultado.isConfirmed){
